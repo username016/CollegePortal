d270aaef1dafbb198ad717b4dbbfb2823db97c81
Ding808 <dingyeuyang@gmail.com>
2024-11-30 17:31:52 -0500

    navigation top bar updated

--- CollegePortal.Services/DataAccessLayer/DbContextStudent.cs
+++ CollegePortal.Services/DataAccessLayer/DbContextStudent.cs
@@ -1,10 +1,4 @@
 ﻿using Microsoft.EntityFrameworkCore;
-using CollegePortal.Entities;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using CollegePortal.Entities.Models;
 
 namespace CollegePortal.Services.DataAccessLayer
@@ -13,16 +7,12 @@ namespace CollegePortal.Services.DataAccessLayer
     {
         public DbContextStudent(DbContextOptions<DbContextStudent> options) : base(options)
         {
-
         }
 
         public DbSet<Student> Students { get; set; }
         public DbSet<GymRoomBookings> GymRoomBookings { get; set; }
         public DbSet<LostAndFound> LostAndFound { get; set; }
-
         public DbSet<StudyRoomBookings> StudyRoomBookings { get; set; }
-
         public DbSet<TutorBookings> TutorBookings { get; set; }
-
     }
 }
