3be7cdfd541e3c2ab3bf3ddeff61788dd7b8c699
Ding808 <dingyeuyang@gmail.com>
2024-11-30 16:17:03 -0500

    The ui part draft

--- CollegePortal.Operations/Program.cs
+++ CollegePortal.Operations/Program.cs
@@ -1,7 +1,10 @@
+using CollegePortal.Services.Repositories;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
 
 var app = builder.Build();
 
